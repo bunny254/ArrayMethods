42a9b4cfe28e62dcc547a723df5d99ff478cf4c0
bunny254 <kimaniwachira03@gmail.com>
2022-03-29 21:36:21 +0300

    Arrays

---
+++ first.js
@@ -0,0 +1,21 @@
+//1. Display the length of an array
+let colors=['red','blue','yellow']
+//console.log(colors);
+console.log(colors.length);
+
+//2. Remove last item in an array
+colors.pop(colors)
+console.log(colors);
+console.log(colors.length);
+
+/*
+//3. Push item into an array (lastIndex)
+colors.push('black');
+console.log(colors);
+console.log(colors.length);
+
+//4. Add new item to array (firstIndex)
+const fruits = ['Apple', 'Orange', 'Strawberry','Peach'];
+fruits.unshift('Mango')
+console.log(fruits);
+console.log(fruits.length); */
